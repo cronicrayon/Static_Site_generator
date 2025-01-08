c0042fc70d05de606c17c111f3cc6ca2ec0f85a5
croniccrayon <jamiemiles12@gmail.com>
2025-01-08 14:41:21 +0000

    added a text_node_to_html_node method and cleaned up all other files

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from textnode import TextNode, TextType
 
+
 def main():
-    dummy_node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(dummy_node)
+    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(node)
+
 
 main()
